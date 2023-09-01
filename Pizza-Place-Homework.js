5c103432110106077933da875086e368e0dc5880
Kyle Ward <wardkyle77@gmail.com>
2023-09-01 07:38:06 -0500

    Complete Pizza Place Homework

---
+++ Pizza-Place-Homework.js
@@ -0,0 +1,32 @@
+let pizzaPlace, numberOfToppings;
+
+pizzaPlace = "Kdubs Always Extra Cheese";
+numberOfToppings = 1; // Just extra cheese!
+console.log(`${pizzaPlace}:`, typeof pizzaPlace, `${numberOfToppings}:`, typeof numberOfToppings);
+console.log(`${pizzaPlace} is my favorite place to get pizza, they always include ${numberOfToppings} extra cheese topping free of charge!`);
+
+if(numberOfToppings < 10){
+  console.log(`Quality, not quantity`);
+}
+else {
+  console.log(`A whole of of pizza`);
+}
+
+// Switch:case
+
+switch(numberOfToppings){
+  case 1: console.log(`Quality, not quantity.`); break;
+  default : console.log(`A whole of of pizza.`); break;
+}
+
+// Ternary Operators
+
+numberOfToppings < 10 ? console.log(`Quality, not quantity.`) : console.log(`A whole of of pizza.`);
+
+// Loop even numbers
+
+let i=1;
+while (i<=numberOfToppings){
+  (i % 2 === 0) ? console.log(i):console.log(`Not an even number`);
+  i++;
+}
